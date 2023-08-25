ffb64530eb42b763814497fedab308cbd2684eea
Sophia Zhang <ssz.windyskies@gmail.com>
2023-08-25 15:00:25 -0700

    FileToString()

--- FileWriter.java
+++ FileWriter.java
@@ -1,11 +1,16 @@
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.PrintWriter;
 
 public class FileWriter {
 
-    public static void main (String [] args) throws FileNotFoundException
+    public static void main (String [] args) throws IOException
     {
         writeToFile ();
+        System.out.println ("\n\n\n");
+        System.out.println (fileToString());
     }
     
     public static void writeToFile () throws FileNotFoundException
@@ -14,5 +19,19 @@ public class FileWriter {
         writer.println("Hello");
         writer.println("World");
         writer.close();
+    }
+
+    public static String fileToString () throws IOException
+    {
+            BufferedReader reader = new BufferedReader(new FileReader ("ReadFromThis.txt"));
+            StringBuilder sB = new StringBuilder ("");
+
+            while (reader.ready())
+            {
+                sB.append ((char)reader.read());
+            }
+            reader.close();
+
+            return sB.toString();
     }
 }
